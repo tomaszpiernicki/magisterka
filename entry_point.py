d1f5f3ceb7b92e092b97779ddeebdde6b178fd75
tomaszpiernicki <tpiernik@gmail.com>
2019-10-17 20:05:21 +0200

    Plik wejściowy
    Signed-off-by: tomaszpiernicki <tpiernik@gmail.com>

---
+++ entry_point.py
@@ -0,0 +1,4 @@
+from gen import Generator
+
+if __name__ == '__main__':
+    Generator.generate_notes_array()
--- gen/Generator.py
+++ gen/Generator.py
@@ -3,60 +3,57 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-from gen import SilenceTools
+from gen.SilenceTools import SilenceTools
 from gen.Note import Note
 from gen.NoteFactory import NoteFactory
 from gen.NoteLength import NoteLength
 
-class Genererator:
+def generate_note(note: Note, period):
+    y, sr = librosa.load(note.sound_file)
+    y = SilenceTools.cleanUp(y)
+    multiplier = note.sound_length.value[0]
+    real_length = multiplier * period
+    y_real_length = real_length * sr
 
-    def generate_note(self, note: Note):
-        y, sr = librosa.load(note.sound_file)
-        y = SilenceTools.SilenceTools.cleanUp(y)
-        multiplier = note.sound_length.value[0]
-        real_length = multiplier * self.period
-        y_real_length = real_length * sr
+    if(len(y) >= y_real_length ):
+        y = y[:int(y_real_length)]
+    else:
+        np.pad(y, (0, y_real_length - len(y)))
 
-        if(len(y) >= y_real_length ):
-            y = y[:int(y_real_length)]
-        else:
-            np.pad(y, (0, y_real_length - len(y)))
-
-        return y, sr
+    return y, sr
 
 
-    def generate_notes_array(self):
-        bpm = 100
-        self.period = 60 / bpm
-        note = NoteLength.Quarter
-        notes_stereotypes = [note]
-        metrum = [4, 4]
-        tacts_to_generate = 2
+def generate_notes_array():
+    bpm = 100
+    period = 60 / bpm
+    note = NoteLength.Quarter
+    notes_stereotypes = [note]
+    metrum = [4, 4]
+    tacts_to_generate = 2
 
+    quarter_real_time = 60 / bpm
+    quarters_in_tact = 4
+    quarters_to_fill = quarters_in_tact * tacts_to_generate
 
-        quarter_real_time = 60 / bpm
-        quarters_in_tact = 4
-        quarters_to_fill = quarters_in_tact * tacts_to_generate
+    noteFactory = NoteFactory()
+    notes_array = noteFactory.generateRandom(quarters_to_fill, notes_stereotypes)
 
-        noteFactory = NoteFactory()
-        notes_array = noteFactory.generateRandom(quarters_to_fill, notes_stereotypes)
+    y = []
+    sr = []
 
-        y = []
-        sr = []
+    for note in notes_array:
+        y_temp, sr_temp = generate_note(note, period)
+        y.append(y_temp)
+        sr.append(sr_temp)
 
-        for note in notes_array:
-            y_temp, sr_temp = self.generate_note(note)
-            y.append(y_temp)
-            sr.append(sr_temp)
+    merged_list = []
+    for arr in y:
+        merged_list += list(arr)
 
-        merged_list = []
-        for arr in y:
-            merged_list += list(arr)
+    merged_list = np.asarray(merged_list)
 
-        merged_list = np.asarray(merged_list)
+    plt.figure()
+    librosa.display.waveplot(merged_list, sr=sr[0])
+    plt.savefig("plot.png")
 
-        plt.figure()
-        librosa.display.waveplot(merged_list, sr=sr[0])
-        plt.savefig("plot.png")
-
-        librosa.output.write_wav("output.wav", merged_list, sr=sr[0])
+    librosa.output.write_wav("output.wav", merged_list, sr=sr[0])
