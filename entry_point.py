16526480f3f37cd2859257085b15c3e0fef8f56c
tomaszpiernicki <tpiernik@gmail.com>
2019-11-09 17:18:44 +0100

    Tworzenie wielu sektogramów

--- entry_point.py
+++ entry_point.py
@@ -1,4 +1,19 @@
+import threading
+
 from gen import Generator
 
+i = 0;
+
+def threadFor(start, stop):
+    for item in range(start, stop):
+        Generator.generate_random_melody()
+        print(item)
+
 if __name__ == '__main__':
-    Generator.generate_notes_array()
+    for item in range(0, 130):
+        Generator.generate_random_melody()
+        print(item)
+
+    # for n in range(0, 500, 50):
+    #     stop = n + 100 if n + 100 <= 1000 else 1000
+    #     threading.Thread(target=threadFor, args=(n, stop)).start())
